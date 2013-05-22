03dd1af391386e0910ee07e2d189bb3f73b0c195
Zaniyar <zaniyar.jahany@gmail.com>
2013-05-22 20:20:32 +0200

    MmsValidate has been implemented..

--- MultiChannel/src/ch/zhaw/multiChannel/model/MmsValidator.java
+++ MultiChannel/src/ch/zhaw/multiChannel/model/MmsValidator.java
@@ -1,16 +1,34 @@
 package ch.zhaw.multiChannel.model;
 
+import java.io.File;
+
 public class MmsValidator {
 	
 	private AttachmentMessage attachmentMessage;
-	
+	public String errorMessage; 
 	public MmsValidator(AttachmentMessage attachmentMessage){
 		
 		this.attachmentMessage = attachmentMessage;
 	}
 	
 	public boolean validate(){
-		return true;
+		return validateAttachement();
 	}
 
+	private boolean validateAttachement(){
+			
+		for(File currentFile : attachmentMessage.getAttachments()){
+			if(currentFile.canExecute()){
+				errorMessage="Exe können nicht verschickt werden!";
+				return false;
+			}
+			if(currentFile.length() > 5242880){
+				errorMessage="File ist grösser als 5MB!";
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
 }
