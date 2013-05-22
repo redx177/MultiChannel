a6e14783ae5ede8c3882ba146bd2f004bfc79736
pwrangel <angelbuterfly@hotmail.com>
2013-05-22 19:18:50 +0200

    added AttachmentMessage

---
+++ MultiChannel/src/AttachmentMessage.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class AttachmentMessage {
+
+	private File[] attachments;
+
+	public File[] getAttachments() {
+		return attachments;
+	}
+
+	public AttachmentMessage(File[] attachments) {
+
+		this.attachments = attachments;
+	}
+	
+
+
+}
