78a1c15568ca39e9247fa2f18c06b25ea42ac368
pwrangel <angelbuterfly@hotmail.com>
2013-05-22 21:01:34 +0200

    SmsValidator Size implemented

--- MultiChannel/src/ch/zhaw/multiChannel/model/SmsValidator.java
+++ MultiChannel/src/ch/zhaw/multiChannel/model/SmsValidator.java
@@ -19,8 +19,13 @@ public class SmsValidator {
 	private boolean validateReceivers() {
 		int numberOfReceiver = message.getReceivers().length;
 		String[] receivers = message.getReceivers();
+		String message = this.message.getMessage();
 		for (int i = 0; i <= numberOfReceiver; i++) {
 			if (receivers[i] == null || !isInteger(receivers[i])) {
+				return false;
+			}
+			if (message.length() > 160) {
+				System.out.println("The message size exceedes the accepted size");
 				return false;
 			}
 
